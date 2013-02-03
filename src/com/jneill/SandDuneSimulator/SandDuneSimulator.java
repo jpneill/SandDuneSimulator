6f640d4cd32a483b7664646a9be76473d2bbf990
James Neill <j.neill.inbox@gmail.com>
2013-02-03 07:13:06 +0000

    Added a scrollpane for displaying images

--- src/com/jneill/SandDuneSimulator/SandDuneSimulator.java
+++ src/com/jneill/SandDuneSimulator/SandDuneSimulator.java
@@ -2,25 +2,38 @@ package com.jneill.SandDuneSimulator;
 
 import javafx.application.*;
 import javafx.event.*;
+import javafx.geometry.Pos;
 import javafx.scene.*;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.stage.*;
 
 public class SandDuneSimulator extends Application{
-	public static void main(String[] args){
-		launch(args);
-	}	
-
+	private Button btn;
+	private HBox rootBox;
+	private ScrollPane sp;
+	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		primaryStage.setTitle("Sand Dune Sim");
-		Button btn = new Button();
+		
+		btn = new Button();
 		btn.setText("This is a button.");
-		StackPane root = new StackPane();
-		root.getChildren().add(btn);
-		primaryStage.setScene(new Scene(root, 400, 400));
+		
+		sp = new ScrollPane();
+		sp.setPrefHeight(200);
+		sp.setVmax(200);
+		
+		rootBox = new HBox(10);
+		rootBox.setAlignment(Pos.CENTER);		
+		rootBox.getChildren().addAll(btn, sp);
+		
+		primaryStage.setScene(new Scene(rootBox, 400, 400));
 		primaryStage.show();
 	}
 		
+
+	public static void main(String[] args){
+		launch(args);
+	}
 }
